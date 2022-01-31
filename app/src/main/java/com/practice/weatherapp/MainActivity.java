b93d94f02a7e117bfde78e8eb5615d36f991a3c3
Saurav singh <sauravsingh85079@gmail.com>
2022-02-01 00:35:19 +0530

    Add Weather Model and Adapter File

--- app/src/main/java/com/practice/weatherapp/MainActivity.java
+++ app/src/main/java/com/practice/weatherapp/MainActivity.java
@@ -1,14 +1,41 @@
 package com.practice.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RelativeLayout homeRL;
+    private ProgressBar loadingPB;
+    private TextView CityNameTV,tempTV,conditionTV;
+    private RecyclerView weatherRV;
+    private TextInputEditText cityEdt;
+    private ImageView backIV,iconIV,searchIV;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        homeRL = findViewById(R.id.rl_Home);
+        loadingPB = findViewById(R.id.pb_loading);
+        CityNameTV = findViewById(R.id.txt_CityName);
+        tempTV = findViewById(R.id.txt_Temp);
+        conditionTV = findViewById(R.id.txt_Condion);
+        weatherRV = findViewById(R.id.Rv_Weather);
+        cityEdt = findViewById(R.id.et_City);
+        backIV = findViewById(R.id.img_Back);
+        iconIV = findViewById(R.id.img_Icon);
+        searchIV = findViewById(R.id.img_search);
     }
 }
---
+++ app/src/main/java/com/practice/weatherapp/WeatherModel.java
@@ -0,0 +1,47 @@
+package com.practice.weatherapp;
+
+public class WeatherModel {
+    private String time;
+    private String temp;
+    private String icon;
+    private String windSpeed;
+
+    public WeatherModel(String time, String temp, String icon, String windSpeed) {
+        this.time = time;
+        this.temp = temp;
+        this.icon = icon;
+        this.windSpeed = windSpeed;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getTemp() {
+        return temp;
+    }
+
+    public void setTemp(String temp) {
+        this.temp = temp;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(String windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+}
