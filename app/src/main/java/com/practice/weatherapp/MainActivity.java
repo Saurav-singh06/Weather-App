cef8a5fc81436e97f108bc0f180b1e7bf24d0557
Saurav singh <sauravsingh85079@gmail.com>
2022-02-02 00:19:34 +0530

    Add Api and Permissions

--- app/src/main/java/com/practice/weatherapp/MainActivity.java
+++ app/src/main/java/com/practice/weatherapp/MainActivity.java
@@ -1,19 +1,36 @@
 package com.practice.weatherapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 
 import org.w3c.dom.Text;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,12 +42,17 @@ public class MainActivity extends AppCompatActivity {
     private ImageView backIV,iconIV,searchIV;
     private ArrayList<WeatherModel> weatherModelArrayList;
     private WeatherAdapter weatherAdapter;
+    private LocationManager locationManager;
+    private int PERMISSION_CODE = 1;
+    private String cityName;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
         setContentView(R.layout.activity_main);
         homeRL = findViewById(R.id.rl_Home);
         loadingPB = findViewById(R.id.pb_loading);
@@ -43,14 +65,72 @@ public class MainActivity extends AppCompatActivity {
         iconIV = findViewById(R.id.img_Icon);
         searchIV = findViewById(R.id.img_search);
 
-        weatherModelArrayList new ArrayList<>();
+        weatherModelArrayList = new ArrayList<>();
         weatherAdapter = new WeatherAdapter(this,weatherModelArrayList);
         weatherRV.setAdapter(weatherAdapter);
 
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED ) {
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},PERMISSION_CODE);
+        }
+        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        cityName = getCityName(location.getLongitude(),location.getLatitude());
+
+        getWeatherInfo(cityName);
+
+        searchIV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String city = cityEdt.getText().toString();
+                if (city.isEmpty()){
+                    Toast.makeText(MainActivity.this,"Please enter city Name",Toast.LENGTH_SHORT).show();
+                }else {
+                    CityNameTV.setText(cityName);
+                    getWeatherInfo(city);
+                }
+            }
+        });
 
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_CODE){
+            if (grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this,"Permissions granted..",Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(this,"Please Provide the permissions ",Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    private String getCityName (double longitude , double latitude){
+        String cityName ="Not Found";
+        Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
+        try {
+            List<Address> addresses = gcd.getFromLocation(latitude,longitude,10);
+
+            for (Address adr : addresses){
+                if (adr != null){
+                    String city = adr.getLocality();
+                    if (city!=null && !city.equals("")){
+                        cityName = city;
+                    }else {
+                        Log.d("Tag","City Not Found");
+                        Toast.makeText(this,"User City Not Found", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return cityName;
+    }
+
     private  void  getWeatherInfo(String cityNmae){
+    String url = "http://api.weatherapi.com/v1/current.json?key=13b2dd0fb522481e9d0183358223101&q="+cityNmae+"&aqi=no";
 
     }
 }
